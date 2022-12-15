2372c00546e6c118effc6a589c01695f3da0b968
Mathieu <matrobin98@gmail.com>
2022-12-14 22:20:53 -0500

    First iteration of display changes

--- dashboard_projet/app.r
+++ dashboard_projet/app.r
@@ -5,25 +5,46 @@ library(ggplot2)
 library(dplyr)
 library(DT)
 library(tidyverse)
+library(ggthemr)
+library(treemapify)
+library(ggrepel)
+library(ggfittext)
 
+ggthemr('pale')
 
 # we have a tabulation separator
 campaign_data <- read.csv('data/marketing_campaign.csv', sep='\t')
 
 # add age category column
-campaign_data['Age'] <- 2022 - campaign_data['Year_Birth']
+campaign_data['Age'] <- 2019 - campaign_data['Year_Birth']
 campaign_data <- campaign_data %>%
   mutate(Age_Category = case_when(
-    Age <= 14 ~ 'Children',
-    Age <= 24 ~ 'Youth',
-    Age <= 64 ~ 'Adult',
-    Age >= 65 ~ 'Senior',
+    Age <= 35 ~ '35 and -',
+    Age <= 45 ~ '36-45',
+    Age <= 55 ~ '46-55',
+    Age <= 65 ~ '55-65',
+    Age > 65 ~ '66 and +'
   ))
+
+# add income range
+campaign_data <- campaign_data %>%
+  mutate(Income_Range = case_when(
+    Income < 10000 ~ '0 - 10k',
+    Income < 20000 ~ '10k - 20k',
+    Income < 45000 ~ '20k - 45k',
+    Income < 60000 ~ '45k - 60k',
+    Income < 75000 ~ '60k - 75k',
+    Income >= 75000 ~ '75k+',
+  ))
+
+campaign_data['Total_childrens'] <- campaign_data['Kidhome'] + campaign_data['Teenhome']
+
 
 # cleaning data
 # Change Alone to single and remove YOLO and absurd
 campaign_data["Marital_Status"][campaign_data["Marital_Status"] == "Alone"] <- "Single"
 campaign_data <- campaign_data[!(campaign_data$Marital_Status %in% c("YOLO", "Absurd")), ]
+campaign_data <- campaign_data[complete.cases(campaign_data), ]
 
 
 age_category_list <- unique(campaign_data$Age_Category)
@@ -55,109 +76,12 @@ ui <- grid_page(
     area = "area1",
     tabsetPanel(
       tabPanel(
-        title = "POC",
-        grid_container(
-          layout = c(
-            "title_demographic title_demographic title_demographic",
-            "area2             area3             area0            ",
-            "area4             area4             area0            "
-          ),
-          row_sizes = c(
-            "0.27fr",
-            "1.22fr",
-            "1.51fr"
-          ),
-          col_sizes = c(
-            "1.58fr",
-            "0.96fr",
-            "0.46fr"
-          ),
-          gap_size = "10px",
-          grid_card(
-            area = "area0",
-            title = "Filters (Not meant to work for poc)",
-            tabPanel(
-              title = "My Shiny App",
-              grid_container(
-                layout = "area1",
-                row_sizes = "1.61fr",
-                col_sizes = "1.01fr",
-                gap_size = "10px",
-                grid_card(
-                  area = "area1",
-                  
-                  sliderInput(
-                    inputId = "inputId",
-                    label = "Example Slider",
-                    min = 0L,
-                    max = 10L,
-                    value = 5L,
-                    width = "100%"
-                  ),
-                  textInput(
-                    inputId = "myTextInput",
-                    label = "Text Input",
-                    value = ""
-                  ),
-                  checkboxGroupInput(
-                    inputId = "myCheckboxGroup",
-                    label = "Checkbox Group",
-                    choices = list(
-                      `choice a` = "a",
-                      `choice b` = "b"
-                    )
-                  ),
-                  radioButtons(
-                    inputId = "myRadioButtons",
-                    label = "Radio Buttons",
-                    choices = list(
-                      `choice a` = "a",
-                      `choice b` = "b"
-                    ),
-                    width = "100%"
-                  )
-                )
-              )
-            )
-          ),
-          grid_card_text(
-            area = "title_demographic",
-            content = "Proof of Concept (data not cleaned)",
-            alignment = "start"
-          ),
-          grid_card(
-            area = "area2",
-            plotOutput(
-              outputId = "ex1",
-              width = "100%",
-              height = "400px"
-            )
-          ),
-          grid_card(
-            area = "area3",
-            plotOutput(
-              outputId = "ex3",
-              width = "100%",
-              height = "400px"
-            )
-          ),
-          grid_card(
-            area = "area4",
-            plotOutput(
-              outputId = "ex2",
-              width = "100%",
-              height = "350px"
-            )
-          )
-        )
-      ),
-      tabPanel(
         title = "Demographic",
         grid_container(
           layout = c(
             "title_demographic title_demographic title_demographic",
             "area2             area7             area0            ",
-            "area4             area4             area0            "
+            "area2             area7             area0            "
           ),
           row_sizes = c(
             "0.27fr",
@@ -173,14 +97,17 @@ ui <- grid_page(
           grid_card(
             area = "area0",
             title = "Filters",
-            tabPanel(
-              title = "My Shiny App",
-              grid_container(
-                layout = ".",
-                row_sizes = "1.72fr",
-                col_sizes = "1.01fr",
-                gap_size = "10px"
-              )
+            radioButtons(
+              inputId = "demo_filter_group",
+              label = "Trier par",
+              choices = list(
+                `Âge` = "Age_Category",
+                `Nombre d'enfant` = "Total_childrens",
+                `Éducation` = "Education",
+                `Revenu Annuel` = "Income_Range",
+                `Statut Marital` = "Marital_Status"
+              ),
+              width = "100%"
             )
           ),
           grid_card_text(
@@ -188,9 +115,8 @@ ui <- grid_page(
             content = "Demographic Analysis",
             alignment = "start"
           ),
-          grid_card(area = "area2"),
-          grid_card(area = "area4"),
-          grid_card(area = "area7")
+          grid_card(area = "area2", plotOutput("demoGraph")),
+          grid_card(area = "area7", plotOutput("demoComplain"))
         )
       ),
       tabPanel(
@@ -199,12 +125,12 @@ ui <- grid_page(
           layout = c(
             "title_demographic title_demographic title_demographic",
             "area2             area2             area0            ",
-            "area3             area4             area0            "
+            "area3             area3             area10            "
           ),
           row_sizes = c(
             "0.27fr",
-            "1.73fr",
-            "1fr"
+            "1.15fr",
+            "1.58fr"
           ),
           col_sizes = c(
             "1.27fr",
@@ -215,36 +141,28 @@ ui <- grid_page(
           grid_card(
             area = "area0",
             title = "Filters",
-            tabPanel(
-              title = "My Shiny App",
-              grid_container(
-                layout = "area1",
-                row_sizes = "1.61fr",
-                col_sizes = "1.01fr",
-                gap_size = "10px",
-                grid_card(
-                  area = "area1",
-                  title = "Fig. 1:",
-                  # Select which the column to plot
-                  selectInput(inputId = "place_y", 
-                              label = "Y-axis:",
-                              choices = c("Education" = "Education", 
-                                          "Marital status" = "Marital_Status", 
-                                          "Age category" = "Age_Category"), 
-                              selected = "Age_Category")
-                )
-              )
-            )
+            selectInput(inputId = "place_y", 
+                        label = "Y-axis:",
+                        choices = c("Education" = "Education", 
+                                    "Marital status" = "Marital_Status", 
+                                    "Age category" = "Age_Category"), 
+                        selected = "Age_Category")
+          ),
+          grid_card(
+            area = "area10",
+            title = "Filters",
+            selectInput(inputId = "xpromotionGraph",
+                        label = "Visualisation2_Category",
+                        choices = c("Age_Category", "Income_Category","children_count"),
+                        selected = "Age_Category")
           ),
           grid_card_text(
             area = "title_demographic",
             content = "Marketing Analysis",
             alignment = "start"
           ),
-          grid_card(area = "area2",
-                    plotOutput("placeGraph")),
-          grid_card(area = "area3"),
-          grid_card(area = "area4")
+          grid_card(area = "area2", plotOutput("placeGraph")),
+          grid_card(area = "area3", plotOutput("promotionGraph"))
         )
       ),
       tabPanel(
@@ -253,12 +171,12 @@ ui <- grid_page(
           layout = c(
             "title_demographic title_demographic title_demographic",
             "area8             area8             area0            ",
-            "area9             area4             area0            "
+            "area9             area9             area10            "
           ),
           row_sizes = c(
             "0.27fr",
-            "1.73fr",
-            "1fr"
+            "1.25fr",
+            "1.48fr"
           ),
           col_sizes = c(
             "1.27fr",
@@ -268,56 +186,52 @@ ui <- grid_page(
           gap_size = "10px",
           grid_card(
             area = "area0",
-            title = "Filters",
-            tabPanel(
-              title = "My Shiny App",
-              grid_container(
-                layout = "area1",
-                row_sizes = "1.61fr",
-                col_sizes = "1.01fr",
-                gap_size = "10px",
-                grid_card(
-                  area = "area1",
-                  title = "Fig. 1:",
-                  # Select which the age category to plot, select all by default
-                  checkboxGroupInput(inputId = "selected_age",
-                                     label = "Select the age category:",
-                                     choices = age_category_list,
-                                     selected = age_category_list),
-                  
-                  # Select which the education level to plot, select all by default
-                  checkboxGroupInput(inputId = "selected_education",
-                                     label = "Select the education level:",
-                                     choices = education_list,
-                                     selected = education_list),
-                  
-                  # Select which the marital status to plot, select all by default
-                  checkboxGroupInput(inputId = "selected_marital_status",
-                                     label = "Select the marital status:",
-                                     choices = marital_status_list,
-                                     selected = marital_status_list)
-                )
-              )
-            )
+            title = "Fig. 1:",
+            checkboxGroupInput(inputId = "selected_age",
+                               label = "Select the age category:",
+                               choices = age_category_list,
+                               selected = age_category_list),
+            
+            # Select which the education level to plot, select all by default
+            checkboxGroupInput(inputId = "selected_education",
+                               label = "Select the education level:",
+                               choices = education_list,
+                               selected = education_list),
+            
+            # Select which the marital status to plot, select all by default
+            checkboxGroupInput(inputId = "selected_marital_status",
+                               label = "Select the marital status:",
+                               choices = marital_status_list,
+                               selected = marital_status_list)
+          ),
+          grid_card(
+            area = "area10",
+            title = "Fig. 2:",
+            selectInput(inputId = "x", 
+                        label = "Visualisation1_Category",
+                        choices = c("Age_Category", "Income_Range"), 
+                        selected = "Age_Category"),
+            
+            
+            selectInput(inputId = "y", 
+                        label = "Visualisation1_Product_type",
+                        choices = c("mean_MntWines","mean_MntFruits","mean_MntMeatProducts","mean_MntFishProducts","mean_MntSweetProducts","mean_MntGoldProds"), 
+                        selected = "mean_MntWines")
           ),
           grid_card_text(
             area = "title_demographic",
             content = "Sales Analysis",
             alignment = "start"
           ),
-          grid_card(area = "area4"),
           grid_card(area = "area8", 
                     plotOutput("amountGraph")),
-          grid_card(area = "area9")
+          grid_card(area = "area9",
+                    plotOutput("consumptionGraph"))
         )
       )
     )
   ),
-  grid_card_text(
-    area = "area2",
-    content = "Logo",
-    alignment = "center"
-  )
+  img(src="logo.jpg", width="130px", height="80px")
 )
 
 server <- function(input, output) {
@@ -325,46 +239,55 @@ server <- function(input, output) {
   data <- read.csv('data/marketing_campaign.csv', sep='\t')
   
   
-  output$ex1 <- renderPlot({
-    ggplot(data = data, mapping = aes(x = Income, y = MntWines,
-                                      color = Marital_Status)) +
-      geom_point() +
-      labs(x = "Income ($)",
-           y = "Money spent on wines ($)",
-           title = "Example 1") +
-      xlim(0, 105000) +
-      theme_minimal()
+  ###Demographic plots
+  
+  ##Graph répartition Consommateurs
+  demo_per_filter_graph <- reactive({
+    #Remove any N/A data from selected column
+    campaign_data <- campaign_data[!(is.na(campaign_data[input$demo_filter_group])),]
+    
+    
+    #Make proportion from selected column
+    campaign_data %>%
+      group_by_at(input$demo_filter_group) %>%
+      summarise(total = n()) %>%
+      mutate(proportion = (total / sum(total))*100)
   })
   
-  output$ex2 <- renderPlot({
-    ggplot(data = data, mapping = aes(x = factor(Education, 
-             level=c('Basic', 'Graduation', '2n Cycle', 'Master', 'PhD')), 
-             y = NumWebVisitsMonth, fill = Education)) +
-      geom_violin() +
-      geom_point(size = 0.5, position = position_jitter(width = 0.1)) +
-      guides(fill = "none") +
-      labs(x = "Level of education",
-           y = "Number of monthly visits to the website",
-           title = "Example 2") +
-      theme_minimal()
+  output$demoGraph <- renderPlot({
+    ggplot(demo_per_filter_graph(), 
+           mapping = aes_string(
+             x = input$demo_filter_group,
+             y = 'proportion',
+             fill = input$demo_filter_group
+           )) +
+      geom_col() +
+      labs(title= "Répartition des comsommateurs", y = "%")
   })
   
-  data_ex3 <- data %>%
-    group_by(Marital_Status, Education) %>%
-    summarize(percCmp1 = mean(AcceptedCmp1))
   
-  output$ex3 <- renderPlot({
-    ggplot(data = data_ex3,
-           mapping = aes(x = Marital_Status, y = Education, 
-                         fill = percCmp1)) +
-      geom_tile() +
-      labs(x = "Marital Status", y = "Education level",
-           title = "Example 3",
-           fill = "Accepted 
-promotion 
-rate") +
-      coord_equal() +
-      theme_minimal()
+  ##Graph répartition Consommateurs s'ayant plain
+  demo_per_filter_complain <- reactive({
+    #Remove any N/A data from selected column
+    
+    campaign_data <- filter(campaign_data, Complain == 1)
+    
+    #Make proportion from selected column
+    campaign_data %>%
+      group_by_at(input$demo_filter_group) %>%
+      summarise(total = n()) %>%
+      mutate(proportionC = (total / sum(total))*100)
+  })
+  
+  output$demoComplain <- renderPlot({
+    ggplot(demo_per_filter_complain(), 
+           mapping = aes_string(
+             x = input$demo_filter_group,
+             y = 'proportionC',
+             fill = input$demo_filter_group
+           )) +
+      geom_col() +
+      labs(title= "Répartition des comsommateurs qui se sont plain.", y = "%")
   })
   
   ###### Marketing places
@@ -422,9 +345,10 @@ rate") +
   # plot the graph
   output$amountGraph <- renderPlot({
     ggplot(data = product_data_percentage()) +
-      geom_pointrange(mapping = aes(x = reorder(MntCategory, -total), y = total, ymin = 0, ymax = total)) +
+      geom_pointrange(mapping = aes(x = reorder(MntCategory, -total), y = total, ymin = 0, ymax = total, color = reorder(MntCategory, -total)), linewidth = 2, size = 3) +
       labs(title="Percentage of amount spent on each category",
-           x ="Category", y = "Total Expenses")
+           x ="Category", y = "Total Expenses") +
+      theme(legend.position="none")
   })
   
   output$table <- DT::renderDataTable({
@@ -434,7 +358,61 @@ rate") +
                   rownames = FALSE)
   })
   
-               
+  #### Consumption graph
+  df1<-campaign_data 
+  
+  habitude_consommation <-reactive({ 
+    df1 %>%group_by_(input$x)  %>%
+      summarize(mean_MntWines = 
+                  mean(MntWines),
+                mean_MntFruits = mean(MntFruits),
+                mean_MntMeatProducts = mean(MntMeatProducts),
+                mean_MntFishProducts = mean(MntFishProducts),
+                mean_MntSweetProducts = mean(MntSweetProducts),
+                mean_MntGoldProds = mean(MntGoldProds)
+      ) })
+  
+  output$consumptionGraph <- renderPlot({
+    ggplot(habitude_consommation(), aes_string(area = input$y, fill = input$y, label = input$x)) +
+      geom_treemap() +
+      geom_treemap_text(colour = "black",
+                        place = "centre",
+                        reflow = TRUE,
+                        size = 17) +
+      labs(title="Habitude de consommation par type de client")
+  })
+  
+  #### Promotion graph
+  
+  df2 <- campaign_data
+  
+  df2$campaign_accepted <- df2$AcceptedCmp1 | df2$AcceptedCmp2 | df2$AcceptedCmp3 | df2$AcceptedCmp4 | df2$AcceptedCmp5
+  
+  df2$children_count <- rowSums(df2[, c("Kidhome", "Teenhome")])
+  
+  accepted_percentage_campaign <- reactive({
+    
+    df2 %>% group_by_(input$xpromotionGraph) %>%
+      summarise(total_accepted = sum(campaign_accepted),
+                total_per_category = n()) %>%
+      mutate(Pourcentage = as.integer(total_accepted / total_per_category*100))
+    
+  })
+  
+  
+  
+  output$promotionGraph<- renderPlot({
+    
+    
+    
+    ggplot( accepted_percentage_campaign(),aes_string(x=input$xpromotionGraph, y='Pourcentage')) +
+      geom_segment( aes_string(xend=input$xpromotionGraph, yend=0)) +
+      geom_point( size=4, color="orange") +coord_flip() +
+      theme_bw() +
+      xlab("")+labs(x=input$xpromotionGraph,y='Pourcentage',
+                    title="Pourcentage à répondre aux réductions de prix")
+    
+  })
 }
 
 shinyApp(ui, server)
